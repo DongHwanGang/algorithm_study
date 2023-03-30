c90c2b6ebc31952a07137af8a40e95f925414434
donghwan <gangdonghwan980@gmail.com>
2023-03-31 00:34:06 +0900

    Add file lecture to bakingdog

--- BAEKJOON/CPP/tempCodeRunnerFile.cpp
+++ BAEKJOON/CPP/tempCodeRunnerFile.cpp
@@ -1,3 +1,42 @@
-    // while(arr.size() != 0){
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <queue>
+using namespace std;
 
-    // }
+int main () {
+    int n;
+    long long int cnt = 0;
+    cin >> n;
+    stack<int> length;
+    queue<int> temp;
+    vector<int> storage(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> storage[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        while(!length.empty()){
+            if(storage[length.top()] < storage[i]){
+                cnt++;
+                length.pop();
+            }
+            else if(storage[length.top()] == storage[i]){
+                cnt++;
+                temp.push(length.top());
+                length.pop();
+            }
+            else{
+                cnt++;
+                break;
+            }
+        }
+        while(!temp.empty()){
+            length.push(temp.front());
+            temp.pop();
+        }
+        length.push(i);
+    }
+    cout << cnt;
+}
