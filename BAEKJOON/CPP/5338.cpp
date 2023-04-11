07adc28db3a2874c1f44f295baeb93068010248a
donghwan <gangdonghwan980@gmail.com>
2023-04-11 23:17:28 +0900

    [BOJ] 5338 마이크로소프트 로고.cpp

---
+++ BAEKJOON/CPP/5338.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|";
+}
