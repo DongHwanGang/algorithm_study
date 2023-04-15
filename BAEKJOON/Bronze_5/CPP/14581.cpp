91ed6a151bac886416db595b95bad1691991331b
donghwan <gangdonghwan980@gmail.com>
2023-04-15 22:02:51 +0900

    [BOJ] 14581 팬들에게 둘러싸인 홍준.cpp

---
+++ BAEKJOON/Bronze_5/CPP/14581.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string pen = "";
+
+    cin >> pen;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << pen << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
